6e33882317a0fcb1ed661a7cdc45c60dfcf725cc
Dormin <zyxster@gmail.com>
2016-10-06 16:39:08 +0200

    Added missing includes to ember.h.

--- include/ember.h
+++ include/ember.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "analyze.h"
+#include "api.h"
 #include "ast.h"
 #include "error.h"
 #include "parse.h"
 #include "scanner.h"
+#include "string.h"
+#include "symbol.h"
 #include "token.h"
